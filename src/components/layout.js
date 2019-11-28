0e71d7fb3e68e5851ff2e53b3f6a666477b3b2b2
Lawrence <Caulker>
2019-11-28 12:19:18 -0500

    initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,21 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import "./bootstrap.min.css"
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
